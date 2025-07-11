dc2bcb8a7f03b7ff2c68f6dde6de84c2b44f3b32
Sharath-yp25 <152462661+Sharath-yp25@users.noreply.github.com>
2025-07-11 12:28:51 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -5,7 +5,7 @@ public class Test {
         int a=50;
         int b=10;
         int res=a+b;
-        System.out.println("the result is " +res)
+        System.out.println("the result is " +res);
     }
     public static void main(String[] args)
     {
